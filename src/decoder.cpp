0817cc19a932f7c76923344491708e5e338e3759
Jacky <lax0xyz@163.com>
2023-10-01 16:50:44 +0800

    加入了frame saver

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -10,7 +10,7 @@ int32_t ff_player::decoder::open(const char *FilePath)
     res = avformat_open_input(&pAVFormatContext, FilePath, nullptr, nullptr);
     if(pAVFormatContext == nullptr)
     {
-        std::clog << "Can't Open the File" << std::endl;
+        qDebug() << "Can't Open the File";
         return -1;
     }
 
@@ -18,7 +18,7 @@ int32_t ff_player::decoder::open(const char *FilePath)
     res = avformat_find_stream_info(pAVFormatContext, nullptr);
     if(res == AVERROR_EOF)
     {
-        std::clog << "Reached File End" << std::endl;
+        qDebug() << "Reached File End";
         close();
         return -1;
     }
@@ -34,43 +34,43 @@ int32_t ff_player::decoder::open(const char *FilePath)
         {
             if((pAVStream->codecpar->width <= 0) || (pAVStream->codecpar->height <= 0))
             {
-                std::clog << "Invalid Video Stream, Stream Index:" << i << std::endl;
+                qDebug() << "Invalid Video Stream, Stream Index:" << i;
                 continue;
             }
             // 查找视频解码器
             pVideoDecoder = const_cast<AVCodec*>(avcodec_find_decoder(pAVStream->codecpar->codec_id));
             if(pVideoDecoder == nullptr)
             {
-                std::clog << "Can't Find Video Codec" << std::endl;
+                qDebug() << "Can't Find Video Codec";
                 continue;
             }
             VideoStreamIndex = i;
-            std::clog << "<Open> Format=" << pAVStream->codecpar->format << ", Frame Size="
-                      << pAVStream->codecpar->width << "*" << pAVStream->codecpar->height << std::endl;
+            qDebug() << "<Open> Format=" << pAVStream->codecpar->format << ", Frame Size="
+                      << pAVStream->codecpar->width << "*" << pAVStream->codecpar->height;
         }
         // 音频流
         else if(pAVStream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO)
         {
             if((pAVStream->codecpar->ch_layout.nb_channels <= 0) || (pAVStream->codecpar->sample_rate <= 0))
             {
-                std::clog << "Invalid Audio Stream, Stream Index:" << i << std::endl;
+                qDebug() << "Invalid Audio Stream, Stream Index:" << i;
                 continue;
             }
             pAudioDecoder = const_cast<AVCodec*>(avcodec_find_decoder(pAVStream->codecpar->codec_id));
             if(pAudioDecoder == nullptr)
             {
-                std::clog << "Can't find Audio Codec" <<std::endl;
+                qDebug() << "Can't find Audio Codec";
                 continue;
             }
             AudioStreamIndex = i;
-            std::clog << "<Open> Format=" << pAVStream->codecpar->format << ", Frame Size="
-                      << pAVStream->codecpar->width << "*" << pAVStream->codecpar->height << std::endl;
+            qDebug() << "<Open> Format=" << pAVStream->codecpar->format << ", Frame Size="
+                      << pAVStream->codecpar->width << "*" << pAVStream->codecpar->height;
         }
     }
 
     if(pAudioDecoder == nullptr || pVideoDecoder == nullptr)
     {
-        std::clog << "<Open> Can't find video or Audio Stream" << std::endl;
+        qDebug() << "<Open> Can't find video or Audio Stream";
         close();
         return -1;
     }
@@ -82,7 +82,7 @@ int32_t ff_player::decoder::open(const char *FilePath)
     pVideoDecodeContext = avcodec_alloc_context3(pVideoDecoder);
     if(pVideoDecodeContext == nullptr)
     {
-        std::clog << "Fail to Alloc a Video Decoder Context" << std::endl;
+        qDebug() << "Fail to Alloc a Video Decoder Context";
         close();
         return -1;
     }
@@ -94,15 +94,16 @@ int32_t ff_player::decoder::open(const char *FilePath)
     res = avcodec_open2(pVideoDecodeContext, nullptr, nullptr);
     if(res != 0)
     {
-        std::clog << "Fail to Open a Video Codec, code " << res << std::endl;
+        qDebug() << "Fail to Open a Video Codec, code " << res;
         close();
         return -1;
     }
+    saver = (frame_saver::get_frame_saver(pVideoDecodeContext));
 
     pAudioDecodeContext = avcodec_alloc_context3(pAudioDecoder);
     if(pAudioDecodeContext == nullptr)
     {
-        std::clog << "Fail to Alloc a Audio Decoder Context" << std::endl;
+        qDebug() << "Fail to Alloc a Audio Decoder Context";
         close();
         return -1;
     }
@@ -113,7 +114,7 @@ int32_t ff_player::decoder::open(const char *FilePath)
     res = avcodec_open2(pAudioDecodeContext, nullptr, nullptr);
     if(res != 0)
     {
-        std::clog << "Fail to Open a Audio Codec, code " << res << std::endl;
+        qDebug() << "Fail to Open a Audio Codec, code " << res;
         close();
         return -1;
     }
@@ -147,6 +148,7 @@ void ff_player::decoder::close()
 int32_t ff_player::decoder::read_frame()
 {
     int res;
+    int i = 0;
     while(true)
     {
         AVPacket* pAVPacket = av_packet_alloc();
@@ -155,12 +157,12 @@ int32_t ff_player::decoder::read_frame()
         res = av_read_frame(pAVFormatContext, pAVPacket);
         if(res == AVERROR_EOF)
         {
-            std::clog << "Reached File End" << std::endl;
+            qDebug() << "Reached File End";
             break;
         }
         else if(res < 0)
         {
-            std::clog << "Fail to Read Frame , code " << res << std::endl;
+            qDebug() << "Fail to Read Frame , code " << res;
             break; 
         }
 
@@ -168,16 +170,17 @@ int32_t ff_player::decoder::read_frame()
         if(pAVPacket->stream_index == VideoStreamIndex)
         {
             AVFrame *pVideoFrame = nullptr;
-            res = decode_packet_to_frame(pVideoDecodeContext, pAVPacket, &pVideoFrame);
+            res = decode_packet_to_frame(pVideoDecodeContext, pAVPacket, &pVideoFrame, i);
             if(res == 0 && pVideoFrame != nullptr)
             {
+                i++;
                 // std::clog << '1' << std::endl;
             }
         }
         else if(pAVPacket->stream_index == AudioStreamIndex)
         {
             AVFrame *pAudioFrame = nullptr;
-            res = decode_packet_to_frame(pAudioDecodeContext, pAVPacket, &pAudioFrame);
+            res = decode_packet_to_frame(pAudioDecodeContext, pAVPacket, &pAudioFrame, -1);
             if(res == 0 && pAudioFrame != nullptr)
             {
                 // std::clog << '2' << std::endl;
@@ -186,10 +189,12 @@ int32_t ff_player::decoder::read_frame()
 
         av_packet_free(&pAVPacket);
     }
+    qDebug() << i << " frames in total";
     return 0;
 }
 
-int32_t ff_player::decoder::decode_packet_to_frame(AVCodecContext *pDecoderContext, AVPacket *pInPacket, AVFrame **ppOutFrame)
+int32_t ff_player::decoder::decode_packet_to_frame(AVCodecContext *pDecoderContext,
+                                                   AVPacket *pInPacket, AVFrame **ppOutFrame, int i)
 {
     AVFrame *pOutFrame  = nullptr;
     int     res;
@@ -198,15 +203,15 @@ int32_t ff_player::decoder::decode_packet_to_frame(AVCodecContext *pDecoderConte
     res = avcodec_send_packet(pDecoderContext, pInPacket);
     if(res == AVERROR(EAGAIN))
     {
-        std::clog << "Buffer Full" << std::endl;
+        qDebug() << "Buffer Full";
     }
     else if(res == AVERROR(EOF))
     {
-        std::clog << "Input File End" << std::endl;
+        qDebug() << "Input File End";
     }
     else if(res < 0)
     {
-        std::clog << "Fail to Send Packet to AVCodec" << std::endl;
+        qDebug() << "Fail to Send Packet to AVCodec";
     }
 
     // 获取解码后的视频或音频帧
@@ -214,26 +219,72 @@ int32_t ff_player::decoder::decode_packet_to_frame(AVCodecContext *pDecoderConte
     res = avcodec_receive_frame(pDecoderContext, pOutFrame);
     if(res == AVERROR(EAGAIN))
     {
-        std::clog << "No Output Data" << std::endl;
+        qDebug() << "No Output Data";
         av_frame_free(&pOutFrame);
         (*ppOutFrame) = nullptr;
         return 0;
     }
     else if(res == AVERROR_EOF)
     {
-        std::clog << "Output File End" << std::endl;
+        qDebug() << "Output File End";
         av_frame_free(&pOutFrame);
         (*ppOutFrame) = nullptr;
         return AVERROR_EOF;
     }
     else if(res < 0)
     {
-        std::clog << "Fail to Receive Frame from AVCodec" << std::endl;
+        qDebug() << "Fail to Receive Frame from AVCodec";
         av_frame_free(&pOutFrame);
         (*ppOutFrame) = nullptr;
         return res;
     }
 
+    if(pInPacket->stream_index == VideoStreamIndex)
+    {
+        saver.save_video_frame(pDecoderContext, i);
+    }
+
     (*ppOutFrame) = pOutFrame;
     return 0;
+}
+
+ff_player::decoder::frame_saver ff_player::decoder::frame_saver::get_frame_saver(AVCodecContext *pVideoDecoderContext)
+{
+    frame_saver saver(pVideoDecoderContext);
+    return saver;
+}
+
+
+void ff_player::decoder::frame_saver::save_video_frame_(AVFrame *pVideoFrame, int width, int height, int frame,
+                                                        const char *file_path)
+{
+    std::string name_ = file_path;
+    name_ += std::to_string(frame);
+    name_ += ".ppm";
+    std::filesystem::path name(name_);
+
+    auto file = new QFile(name);
+    if (!file->open(QIODevice::WriteOnly | QIODevice::Text))
+        return;
+    QTextStream file_out(reinterpret_cast<FILE *>(&file));
+
+    file_out << "P6\n" << width << height << "\n255\n";
+    for(int i = 0; i < height; i++)
+        for(int j = 0; j < 3; j++)
+            file_out << pVideoFrame->data[0] + i * pVideoFrame->linesize[0] + j;
+
+    delete file;
+}
+
+void ff_player::decoder::frame_saver::save_video_frame_test(AVFrame *pVideoFrame, int width, int height, int frame)
+{
+    const char* file_path = R"(D:\Project\C\ffplayer\test\frames)";
+    save_video_frame_(pVideoFrame, width, height, frame, file_path);
+}
+
+void ff_player::decoder::frame_saver::save_video_frame(AVCodecContext* pVideoDecoderContext, int32_t i) const
+{
+    sws_scale(ImgCtx, YuvFrame->data, YuvFrame->linesize, 0,
+              pVideoDecoderContext->height, RgbFrame->data, RgbFrame->linesize);
+    save_video_frame_test(RgbFrame, pVideoDecoderContext->width, pVideoDecoderContext->height, i);
 }
--- src/decoder.h
+++ src/decoder.h
@@ -4,7 +4,13 @@
 #ifndef FF_PLAYER_DECODER_H_
 #define FF_PLAYER_DECODER_H_
 
-#include <iostream>
+#include <filesystem>
+#include <string>
+
+#include <QDebug>
+#include <QFile>
+#include <QString>
+#include <QTextStream>
 
 #ifdef __cplusplus
 extern "C"
@@ -12,6 +18,8 @@ extern "C"
 #endif
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
 #ifdef __cplusplus
 }
 #endif
@@ -40,6 +48,70 @@ namespace ff_player
 
 class decoder
 {
+public:
+    struct frame_saver
+    {
+        struct SwsContext* ImgCtx;
+        AVFrame* RgbFrame;
+        AVFrame* YuvFrame;
+
+        frame_saver() = default;
+        explicit frame_saver(AVCodecContext* pVideoDecoderContext)
+        {
+            RgbFrame = av_frame_alloc();
+            YuvFrame = av_frame_alloc();
+
+            /* 设置数据转换参数
+             * 前三个参数为源地址长宽以及数据格式
+             * 然后的三个参数为目的地址长宽以及数据格式
+             * 算法类型  AV_PIX_FMT_YUVJ420P   AV_PIX_FMT_BGR24
+             * */
+            ImgCtx = sws_getContext(pVideoDecoderContext->width, pVideoDecoderContext->height,
+                                    pVideoDecoderContext->pix_fmt, pVideoDecoderContext->width,
+                                    pVideoDecoderContext->height, AV_PIX_FMT_RGB32, SWS_BICUBIC,
+                                    nullptr, nullptr, nullptr);
+
+            // 分配一帧图像的数据大小
+            int numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGB32, pVideoDecoderContext->width,
+                                                    pVideoDecoderContext->height, 1);
+            auto *out_buffer = (unsigned char *)av_malloc(numBytes * sizeof(unsigned char));
+
+            // 将rgb_frame中的数据关联到pVideoDecoderContext，数据会同步改变
+            av_image_fill_arrays(RgbFrame->data, RgbFrame->linesize, out_buffer, AV_PIX_FMT_RGB32,
+                                 pVideoDecoderContext->width, pVideoDecoderContext->height, 1);
+        }
+
+        ~frame_saver()
+        {
+            av_frame_free(&RgbFrame);
+            av_frame_free(&YuvFrame);
+        }
+
+        static frame_saver get_frame_saver(AVCodecContext* pVideoDecoderContext);
+
+        /**
+         * @brief 保存视频帧到file_path
+         * @param pVideoFrame   要保存的视频帧
+         * @param width         视频帧宽度
+         * @param height        视频帧高度
+         * @param frame         视频帧编号
+         * @param file_path     保存路径
+         * */
+        static void save_video_frame_(AVFrame* pVideoFrame, int width, int height, int frame, const char* file_path);
+
+        /**
+         * @brief 保存视频帧到默认路径
+         * 调用save_video_frame(AVFrame* pVideoFrame, int width, int height, int frame, const char* file_path)
+         * */
+        static void save_video_frame_test(AVFrame* pVideoFrame, int width, int height, int frame);
+
+        /**
+         * @brief 保存一帧图像
+         * @param pVideoDecoderContext  视频帧的相关数据
+         * @param i                     视频帧个数
+         * */
+        void save_video_frame(AVCodecContext* pVideoDecoderContext, int32_t i) const;
+    };
 
 private:
     AVFormatContext *pAVFormatContext;
@@ -47,6 +119,7 @@ private:
     uint32_t        VideoStreamIndex;
     AVCodecContext  *pAudioDecodeContext;
     uint32_t        AudioStreamIndex;
+    frame_saver     saver{};
 
 public:
     decoder()
@@ -62,8 +135,6 @@ public:
     /**
      * @brief 打开并解析媒体文件
      * 注意，open()函数只用来解封装，解码媒体文件请使用read_frame()
-     * @see read_frame()
-     * @see decode_packet_to_frame()
      * @param FilePath  需要解析的文件的路径
      * @retval 0    成功打开并解析文件
      * @retval -1   出现错误，具体错误查看控制台中输出的信息
@@ -77,7 +148,6 @@ public:
 
     /**
      * @brief 读取音视频包，调用decode_packet_to_frame进行解码
-     * @see decode_packet_to_frame()
      * @retval 0 读取结束
      * */
     int32_t read_frame();
@@ -96,9 +166,10 @@ public:
      *      最后几十没有数据包送入了，也要调用avcodec_send_packet()送入空数据包，以驱动解码模块继续解码缓冲区中的数据
      *      此时avcodec_receive_frame()还是会有音视频帧输出
      *      直到返回AVERROR_EOF才表示所有数据包解码完成。
-     * @param pAVCodecContext   输入数据包的相关解码数据
-     * @param pAVPacket         需要解码的数据包
-     * @param pAVFrame          用于接收解码后数据
+     * @param pDecoderContext   输入数据包的相关解码数据
+     * @param pInPacket         需要解码的数据包
+     * @param ppOutFrame        用于接收解码后数据
+     * @param i                 视频帧数目
      * @retval 0                成功解码
      * @retval AVERROR(EAGAIN)  输入端：缓冲区已满，无法接收新的数据
      * @retval AVERROR(EAGAIN)  输出端：输出不可用，需要更多输入才可以输出
@@ -106,7 +177,7 @@ public:
      * @retval AVERROR_EOF      输出端：到达输出文件末尾，所有数据包解码完成
      * @retval -1               无法获得输出流
      * */
-    static int32_t decode_packet_to_frame(AVCodecContext *pAVCodecContext, AVPacket *pAVPacket, AVFrame **pAVFrame);
+    int32_t decode_packet_to_frame(AVCodecContext *pDecoderContext, AVPacket *pInPacket, AVFrame **ppOutFrame, int i);
 
 }; // class decoder
 
